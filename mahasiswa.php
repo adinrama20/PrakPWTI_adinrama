c6b25a5d20cc3fe77ab78b5fefa39082f7e097d3
Adin Rama Ariyanto Putra <rp00038@gmail.com>
2023-04-15 13:05:55 +0700

    menggabungkan file mahasiswa.php dari branch development

---
+++ mahasiswa.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mahasiswa</title>
+</head>
+<body>
+  <h3>Halo, nama saya Adin Rama</h3>
+  <ul>
+    <li>Ini baris 1</li>
+    <li>Ini baris 2</li>
+  </ul>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem quasi rem doloribus perspiciatis quibusdam ducimus?</p>
+</body>
+</html>
